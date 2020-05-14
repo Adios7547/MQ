919a5c96d1c6e9adc073c23126ac9e4a97a9b49d
Andy Kim <hyungSeok.kim@outlook.com>
2020-05-14 21:38:41 +1200

    QRcodeScanner Implemented

--- App.js
+++ App.js
@@ -1,112 +1,81 @@
-'use strict';
-
-import React, { Component } from 'react';
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button
+} from 'react-native';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Linking
-} from 'react-native';
+  BarCodeScanner
+} from 'expo-barcode-scanner';
 
-import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
-
-class ScanScreen extends Component {
-  onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
-    );
+export default class BarcodeScannerExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    scanned: false,
   };
-  //양준석은 바보다
-  render() {
-    return (
-      <React.Fragment>
-        {/* <QRCodeScanner
-          onRead={this.onSuccess}
-          flashMode={RNCamera.Constants.FlashMode.torch}
-          topContent={
-            <Text style={styles.centerText}>
-              Go to{' '}
-              <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-          }
 
-          bottomContent={
-            <TouchableOpacity style={styles.buttonTouchable}>
-              <Text style={styles.buttonText}>OK. Got it!</Text>
-            </TouchableOpacity>
+  async componentDidMount() {
+    this.getPermissionsAsync();
+  }
+
+  getPermissionsAsync = async () => {
+    const {
+      status
+    } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted'
+    });
+  };
+
+  render() {
+    const {
+      hasCameraPermission,
+      scanned
+    } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>
+    }
+    return (
+      <View style={
+        {
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+        }
+      } >
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject} />
+
+        {scanned && (<
+          Button title={
+            'Tap to Scan Again'
           }
-        /> */}
-        <div>양준석은 멍청이다</div>
-      </React.Fragment>
+          onPress={
+            () => this.setState({
+              scanned: false
+            })
+          } />
+        )
+        }</View>
     );
   }
+
+  handleBarCodeScanned = ({
+    type,
+    data
+  }) => {
+    this.setState({
+      scanned: true
+    });
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
 }
-
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
-  },
-  buttonTouchable: {
-    padding: 16
-  }
-});
-
-AppRegistry.registerComponent('default', () => ScanScreen);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { StyleSheet, Text, View, Button } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!!!!!!!</Text>
-//       <Button title="yang" onPress={() => console.log("dsds")}>
-
-//       </Button>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
